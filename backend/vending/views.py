d300972d8393f35d8b8de57cb87b94476d5b5206
bearkaito <kaito.kumakyu@gmail.com>
2023-01-24 11:22:23 +0900

    feat:#17 clear

--- backend/vending/views.py
+++ backend/vending/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .serializers import ProductSerializer, StockSerializer, OrderMethodSerializer, OrderSerializer
+from .models import Product, Stock, Order_Method, Order
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+
+class ProductView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permissions_classes = (AllowAny,)
+
+
+class StockView(generics.ListAPIView):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    permissions_classes = (AllowAny,)
+
+
+class OrderMethodView(generics.ListAPIView):
+    queryset = Order_Method.objects.all()
+    serializer_class = OrderMethodSerializer
+    permissions_classes = (AllowAny,)
+
+
+class OrderView(generics.ListAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permissions_classes = (AllowAny,)
