9b42db4178115da4bd63add20fe70890a041842e
yoshi-non <himekawa.yoshino44@gmail.com>
2023-01-25 17:04:13 +0900

    fix: encoding

--- backend/sql/vending_products.sql
+++ backend/sql/vending_products.sql
@@ -1,4 +1,6 @@
 BEGIN;
+
+\encoding UTF8;
 -- vending_product
 insert into vending_product (name, price, image, is_set, created_at, updated_at) values ('アクエリアス', 100, 'products/product1.png', TRUE, current_timestamp, current_timestamp);
 insert into vending_product (name, price, image, is_set, created_at, updated_at) values ('綾鷹カフェ', 100, 'products/product2.png', TRUE, current_timestamp, current_timestamp);
