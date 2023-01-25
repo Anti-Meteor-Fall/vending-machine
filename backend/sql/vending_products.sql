9083adef50ae673a9d217c963968ed0546277614
yoshi-non <himekawa.yoshino44@gmail.com>
2023-01-25 16:58:25 +0900

    fix: #36

--- backend/sql/vending_products.sql
+++ backend/sql/vending_products.sql
@@ -1,7 +1,7 @@
 BEGIN;
 -- vending_product
-insert into vending_product (name, price, image, is_set, created_at, updated_at) values ('アクエリアス', 100, 'product1', TRUE, current_timestamp, current_timestamp);
-insert into vending_product (name, price, image, is_set, created_at, updated_at) values ('綾鷹カフェ', 100, 'product2', TRUE, current_timestamp, current_timestamp);
+insert into vending_product (name, price, image, is_set, created_at, updated_at) values ('アクエリアス', 100, 'products/product1.png', TRUE, current_timestamp, current_timestamp);
+insert into vending_product (name, price, image, is_set, created_at, updated_at) values ('綾鷹カフェ', 100, 'products/product2.png', TRUE, current_timestamp, current_timestamp);
 
 -- vending_stock
 
