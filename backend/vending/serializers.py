a0d95aac8a1540441eec85b3fb0da3435677cfe8
bearkaito <kaito.kumakyu@gmail.com>
2023-02-07 09:37:14 +0900

    change:APIの内容変更

--- backend/vending/serializers.py
+++ backend/vending/serializers.py
@@ -1,17 +1,18 @@
 from rest_framework import serializers
 from .models import Product, Order_Method, Order
 
+class ProductlistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [
             'id',
-            'name',
-            'price',
-            'is_set',
-            'quantity',
-            'created_at',
-            'updated_at',
+            'quantity'
         ]
 
 class OrderMethodSerializer(serializers.ModelSerializer):
