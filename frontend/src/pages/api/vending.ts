4d526e212874a05b86e1d5da223ce7680970c52f
yoshi-non <himekawa.yoshino44@gmail.com>
2023-02-27 17:56:05 +0900

    商品の一覧データをSSGで取得

--- frontend/src/pages/api/vending.ts
+++ frontend/src/pages/api/vending.ts
@@ -1,3 +1,13 @@
+// 商品一覧の取得
+
+import { Product } from "@/types/api";
+
+export const getProductList = async (): Promise<Product[]> => {
+  const res = await fetch(new URL("http://127.0.0.1:8000/api/vending/productslist/"));
+  const data: Product[] = await res.json();
+  return data;
+};
+
 // バックエンドへ商品在庫の送信
 export const putProductQuantity = (id:number,quantity:number) =>{
   fetch("http://127.0.0.1:8000/api/vending/products/" + id, {
@@ -13,7 +23,7 @@ export const putProductQuantity = (id:number,quantity:number) =>{
   });
 } 
 
-  // バックエンドへ購入方法の送信
+// バックエンドへ購入方法の送信
 export const postOrderMethod = (product_id:number,order_method_id:number) =>{
   fetch("http://127.0.0.1:8000/api/vending/orders/", {
     method: "POST",
---
+++ frontend/src/types/api.ts
@@ -0,0 +1,11 @@
+export type Product = {
+    id: number;
+    name: string;
+    price: number;
+    image: string;
+    is_set: boolean;
+    quantity: number;
+    created_at: string;
+    updated_at: string;
+};
+  
