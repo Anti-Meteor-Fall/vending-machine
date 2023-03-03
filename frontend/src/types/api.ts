fc14cd5abaa2217d905ddf649f0709545b7630a5
Lnida <91710828+d4n1938@users.noreply.github.com>
2023-03-03 12:09:40 +0900

    api,時間表示,バグ修正

--- frontend/src/types/api.ts
+++ frontend/src/types/api.ts
@@ -1,12 +1,12 @@
 export type Product = {
-    id: number;
-    name: string;
-    price: number;
-    image: string;
-    is_set: boolean;
-    quantity: number;
-    created_at: string;
-    updated_at: string;
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+  is_set: boolean;
+  quantity: number;
+  created_at: string;
+  updated_at: string;
 };
 
 // 天気予報
@@ -50,4 +50,4 @@ export type WeatherData = {
     };
     uvIndex?: string;
   }[];
-}
+};
